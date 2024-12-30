94d4ebbdf782252d18b3077120d4f226f64a4cdf
Youssefboulahia1999 <76653310+Youssefboulahia1999@users.noreply.github.com>
2024-12-30 15:53:17 +0100

    Update ScrapingWeb.py

--- ScrapingWeb.py
+++ ScrapingWeb.py
@@ -1,69 +1,65 @@
 import requests
+from bs4 import BeautifulSoup
 
-#le requests peut aussi extrair des page web
+# Récupérer le contenu de la page web
 response = requests.get('https://youssefboulahia1999.github.io/projet2/')
 content = response.content
-# print(content)
 
-#utilise la librairie beaurifulsoup du pack bs4 pour recuperer un element
-from bs4 import BeautifulSoup
-#le parser recupere tous le code
-parser = BeautifulSoup(content,"html.parser")
-#body prend le body du code html
+# Utiliser BeautifulSoup pour analyser le contenu HTML
+parser = BeautifulSoup(content, "html.parser")
+
+# Récupérer le contenu du premier paragraphe <p>
 body = parser.body
-
-# recuperer le p de chaque body
 p = body.p
-#sous le .text il recuper le paragraphe avec la balise <p>
 print(p.text)
 
-#dans le head on recupere le title
+# Récupérer le titre dans la balise <title> dans <head>
 head = parser.head
 title = head.title
 print(title.text)
 
-parser = BeautifulSoup(content, 'html.parser')
+# Trouver tous les éléments <body>
+body_elements = parser.find_all("body")
 
-#obtenir tous les elements de la balise body c'est une liste
-body = parser.find_all("body")
+# Trouver tous les éléments <p> dans <body>
+p_elements = body_elements[0].find_all("p")
+print(p_elements[0].text)
 
-# Tous les elements de la balise <p>
-p = body[0].find_all("p")
-
-print(p[0].text)
-
+# Récupérer le titre de la page depuis <head>
 headr = parser.find_all('head')
-titler =headr[0].find_all('title')
-print(titler.text)
+titler = headr[0].find_all('title')
+print(titler[0].text)
 
-#on recupere l'id firt du paragraphe p
-firt_paragraph = parser.find_all("p", id="first")[0]
+# Récupérer le premier paragraphe avec l'id "first"
+first_paragraph = parser.find_all("p", id="first")[0]
 print(first_paragraph.text)
 
+# Récupérer le second paragraphe avec l'id "second"
 second_paragraph = parser.find_all("p", id="second")[0]
 print(second_paragraph.text)
 
-#Pour le class
-first_class = parser.find_all("p",class_="class1")[0]
+# Récupérer le premier paragraphe avec la classe "class1"
+first_class = parser.find_all("p", class_="class1")[0]
 print(first_class.text)
 
-# Sélectionner tous les élements de la classe first-item
-first items = parser.select(".first-item")
+# Sélectionner tous les éléments de la classe "first-item"
+first_items = parser.select(".first-item")
 print(first_items[0].text)
 
-first class2 text = parser.select(".class2")[0].text
-print(first class2 text)
+# Sélectionner le texte de la première balise avec la classe "class2"
+first_class2_text = parser.select(".class2")[0].text
+print(first_class2_text)
 
-second text = parser.select("#second")[0].text
-print (second text)
+# Sélectionner le texte du paragraphe avec l'id "second"
+second_text = parser.select("#second")[0].text
+print(second_text)
 
-
-# Trouver Le nombre de fautes de Chelsea
+# Trouver le nombre de fautes de Chelsea
 offences = parser.select("#fautes")[0]
-chelsea offences = offences.select ("td")[11]
+chelsea_offences = offences.select("td")[11]
 chelsea_offences_count = chelsea_offences.text
+print(chelsea_offences_count)
 
-print(chelsea offences count)
-
-psg_pass_count = parser.select("#passes")[0].select ("td") [2].text
+# Trouver le nombre de passes de PSG
+psg_pass_count = parser.select("#passes")[0].select("td")[2].text
 print(psg_pass_count)
